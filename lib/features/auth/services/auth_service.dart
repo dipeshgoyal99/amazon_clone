eb8e7ed41b1c70c9ce7cbbdf5d7a981d716df7af
dipesh <dr.dipeshgoyal265@gmail.com>
2023-01-07 21:04:36 +0530

    update

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -3,9 +3,12 @@ import 'dart:convert';
 import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/models/user.dart';
 import 'package:amazon_clone/constants/utils.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:amazon_clone/constants/global_variable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class AuthService {
   // sign up user
@@ -69,8 +72,12 @@ class AuthService {
       httpErrorHandle(
         response: res,
         context: context,
-        onSuccess: () 
-{}
+        onSuccess: () async {
+           SharedPreferences prefs = await SharedPreferences.getInstance();
+      Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+
+        }
           
            
          
@@ -79,4 +86,7 @@ class AuthService {
       showSnackBar(context, e.toString());
     }
   }
+}
+
+class Provider {
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class User {
   final String id;
   final String name;
@@ -31,6 +33,42 @@ class User {
       
     };
   }
+factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['_id'] ?? '',
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+      address: map['address'] ?? '',
+      type: map['type'] ?? '',
+      token: map['token'] ?? '',
+      
+    );
+  }
 
-  toJson() {}
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
+
+  User copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? password,
+    String? address,
+    String? type,
+    String? token,
+    List<dynamic>? cart,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      address: address ?? this.address,
+      type: type ?? this.type,
+      token: token ?? this.token,
+    
+    );
+  }
 }
